4bcdd50c1c44e2b251943406f8414c452e0f8f21
Rick Booth <admin@rickbooth.me>
2021-06-09 20:52:28 +0100

    feat: add projects to timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,4 +1,9 @@
 export const Timeline: React.VFC = (): React.ReactElement => {
+    const externalProps = {
+        target: '_blank',
+        rel: 'noopener noreferrer',
+    }
+
     return (
         <div className="relative">
             <div className="absolute left-0 top-0 bg-gradient-to-br from-nord4 to-nord9 w-full md:w-1 h-1 md:h-full" />
@@ -45,6 +50,46 @@ export const Timeline: React.VFC = (): React.ReactElement => {
                     <p className="text-nord3 mb-1 text-sm">
                         <span className="text-nord9">Skills</span> - Software Architecture, Unit
                         Testing, Integration Testing, Serverless Deployment, Progressive Web Apps
+                    </p>
+                </div>
+                <div className="pb-6 xl:pb-8">
+                    <h3 className="tracking-widest font-bold mb-1">Projects</h3>
+                    <p className="text-nord3 mb-1 text-sm">
+                        <a href="https://github.com/Rixcy/cv" {...externalProps}>
+                            <span className="text-nord9 underline">CV</span>
+                        </a>{' '}
+                        - This project, build using TailwindCSS and Next.JS.
+                    </p>
+                    <p className="text-nord3 mb-1 text-sm">
+                        <a href="https://github.com/Rixcy/community" {...externalProps}>
+                            <span className="text-nord9 underline">Community</span>
+                        </a>{' '}
+                        - Full stack SaaS application for building a community, built using Blitz.
+                    </p>
+                    <p className="text-nord3 mb-1 text-sm">
+                        <a href="https://github.com/Rixcy/nextjs-starter" {...externalProps}>
+                            <span className="text-nord9 underline">NextJS Starter</span>
+                        </a>{' '}
+                        - Starter project for NextJS including Storybook, Lighthouse Audits,
+                        TailwindCSS and more.
+                    </p>
+                    <p className="text-nord3 mb-1 text-sm">
+                        <a href="https://github.com/Rixcy/vite-react-starter" {...externalProps}>
+                            <span className="text-nord9 underline">Vite Starter</span>
+                        </a>{' '}
+                        - Similar to the NextJS starter, but for Vite.
+                    </p>
+                    <p className="text-nord3 mb-1 text-sm">
+                        <a href="https://github.com/Rixcy/rickbooth.me" {...externalProps}>
+                            <span className="text-nord9 underline">rickbooth.me</span>
+                        </a>{' '}
+                        - Current portfolio website built using NextJS and Dracula UI.
+                    </p>
+                    <p className="text-nord3 mb-1 text-sm">
+                        <a href="https://github.com/Rixcy/airbnb-clone" {...externalProps}>
+                            <span className="text-nord9 underline">AirBnB Clone</span>
+                        </a>{' '}
+                        - A homepage clone of AirBnB to practice using TailwindCSS.
                     </p>
                 </div>
             </div>
