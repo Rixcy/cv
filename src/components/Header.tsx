dc58ce072af5d741a86a480d057c17e94ef81f4c
Rick Booth <admin@rickbooth.me>
2022-04-11 13:16:15 +0100

    feat: update job title in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
-export const Header: React.VFC = (): React.ReactElement => {
+export const Header = () => {
     return (
         <>
-            <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-nord1 pb-6 header">
-                RICK BOOTH
+            <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-nord1 pb-6 header uppercase">
+                Rick Booth
             </h1>
             <h2 className="text-nord1 text-sm md:text-base tracking-wider pl-1 pb-6">
-                Front-End Development Tech Lead
+                Senior Frontend Engineer
             </h2>
         </>
     )
